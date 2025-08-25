8b6c1d9b77615b6e2f943d475c0616d052ed6e0c
msweier <mitchell.s.weier@usace.army.mil>
2025-08-25 10:55:20 -0500

    update datetime requierment for versions

--- src/loadNwsCHPS/loadNwsCHPS.py
+++ src/loadNwsCHPS/loadNwsCHPS.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 import cwms
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import json
 import pandas as pd
 import xml.etree.ElementTree as ET
@@ -45,7 +45,9 @@ def parse_nws_loc_parm_data(locId, param):
 
     if ('LOC' in locId[5::])  & ("Flow" in param):
         param = 'Flow-Local'
-
+        
+    if ('ROR' in locId[5::])  & ("Flow" in param):
+        param = param +'-RainOnPool' 
 
     if ('NON' in locId) & (len(locId) > 5):
         param = param +'-Non_contrib'
@@ -112,13 +114,13 @@ def load_chps_data(file_path):
             df["date-time"] = pd.to_datetime(df["date-time"])
             if versionDateTime:
                 data_json = cwms.timeseries_df_to_json(data = df, 
-                                                   ts_id = ts_id, units = nws_units, office_id = OFFICE, version_date= versionDateTime)
+                                                    ts_id = ts_id, units = nws_units, office_id = OFFICE, version_date= versionDateTime)
             else:
                 data_json = cwms.timeseries_df_to_json(data = df, 
-                                                   ts_id = ts_id, units = nws_units, office_id = OFFICE)
+                                                    ts_id = ts_id, units = nws_units, office_id = OFFICE)
             logging.info(f'Storing {nws_locationId} {nws_parameterId} as {ts_id} with {nws_units} units and version {versionDateTime}')
             cwms.store_timeseries(data=data_json)
-            #print(ts_id, nws_id, nws_locationId, nws_parameterId, usace_parameterId, nws_units, nws_creationDate, nws_creationTime, versionDateTime)
+        #print(ts_id, nws_id, nws_locationId, nws_parameterId, usace_parameterId, nws_units, nws_creationDate, nws_creationTime, versionDateTime)
         
         except:
             print(f'***CWMS location not found for {nws_id}....skipping')
@@ -148,7 +150,7 @@ load_dotenv()
 APIROOT = os.getenv("API_ROOT")
 OFFICE = os.getenv("OFFICE")
 APIKEY = os.getenv('API_KEY')
-APIROOT
+
 
 # api key needs to have the test "apikey" as a prefix to work with CDA 
 apiKey = "apikey " + APIKEY
@@ -174,7 +176,8 @@ missingCwmsValue = -340282346638528859811704183484516925440
 CWMS_Ver_Base = "Fcst-NCRFC-CHIPS"  
 
 # get cwms location group to link NWS station ID
-Locdf = cwms.get_location_group(loc_group_id  = "NWS Handbook 5 ID",category_id= "Agency Aliases", office_id = "CWMS").df.set_index('location-id')
+Locdf = cwms.get_location_group(loc_group_id  = "NWS Handbook 5 ID",category_id= "Agency Aliases", 
+                                category_office_id= "CWMS", group_office_id="CWMS", office_id=OFFICE).df.set_index('location-id')
 
 
 
@@ -197,23 +200,28 @@ nws_alias
 
 
 # grab the location group of RFC CHPS Aliases
-tmp_df = cwms.get_location_group(loc_group_id  = "Locations",category_id= "RFC CHPS Aliases", office_id = "MVP").df.set_index('location-id')
-#Grab all of the locations that have a eccc station number assigned to them
+tmp_df = cwms.get_location_group(loc_group_id  = "Locations",category_id= "RFC CHPS Aliases", 
+                                 category_office_id=OFFICE, group_office_id=OFFICE, office_id = OFFICE).df.set_index('location-id')
+#Grab all of the locations that have a nws ID assigned to them
 tmp_df=tmp_df[tmp_df['alias-id'].notnull()]
 
 #rename the columns
 tmp_df = tmp_df.rename(columns = {'alias-id': 'NWS_ID','attribute':'Loc_attribute'})
 
-# drop the _CHPS suffix
+# # drop the _CHPS suffix
 tmp_df['NWS_ID'] = tmp_df['NWS_ID'].str.slice(0, -5)
-nws_alias.update(tmp_df)
+tmp_df['Loc_attribute']=0
+
+# drop the rows to be replaced
+nws_alias = nws_alias.drop(nws_alias.index.intersection(tmp_df.index))
+# replace the rows
+nws_alias = pd.concat([nws_alias, tmp_df])
 
 # find duplicates
 duplicates = nws_alias[nws_alias['NWS_ID'].duplicated(keep=False)]
 
 #drop duplicates that are sublocations
 rows_to_drop = duplicates.index[duplicates.index.str.contains('-')]
-
 
 nws_alias = nws_alias.drop(rows_to_drop)
 
@@ -222,7 +230,7 @@ nws_alias = nws_alias.drop(rows_to_drop)
 
 
 #print('%s being processed' %(filename))
-Filetime = filename.split(".")[1]
+Filetime = filename.split(".")[-1]
 fmt = '%Y%m%d%H%M%S' 
 fDateTime = datetime.strptime(Filetime, fmt)
 if 'auto' in filename:
@@ -238,8 +246,9 @@ elif 'CRF' in filename:
 else:
     fileBaseName = filename.split('MSR_main_')[1].split('.')[0]
     # set version datetime to current date and 0111 time to avoid too many versions
-    versionDateTime = fDateTime.replace(hour=1, minute=11, second=0)
-    versionDateTime = versionDateTime.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+    versionDateTime = fDateTime.replace(hour=1, minute=11, second=0,  tzinfo=timezone.utc)
+
+    # versionDateTime = versionDateTime.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
     #versionDate = fDateTime.strftime(f'%d%b%Y, {versionTime}')
     # standard text id to save last runtime to
     textKey = 'cv_'+fileBaseName[0:12]
